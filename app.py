6115fa3e147458f4e4a233d169ee7fa8d1ddbbf9
pandeadwait <pandeadwait21@gmail.com>
2025-07-26 00:02:50 +0530

    implemented methods to create and store embeddings

--- app.py
+++ app.py
@@ -1,6 +1,37 @@
 import streamlit as st
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import HuggingFaceInstructEmbeddings
+from langchain.vectorstores import FAISS
+
+def getPdfText(pdfDocs):
+    text = ""
+
+    for pdf in pdfDocs:
+        reader = PdfReader(pdf)
+
+        for page in reader.pages:
+            text += page.extract_text()
+
+    return text
+
+def getTextChunks(rawText):
+    textSplitter = CharacterTextSplitter(separator = '\n', chunk_size = 1000, chunk_overlap = 200, length_function = len)
+
+    chunks = textSplitter.split_text(rawText)
+
+    return chunks
+
+def getVectorstore(textChunks):
+    embeddings = HuggingFaceInstructEmbeddings(model_name = "hkunlp/instructor-xl")
+    vectorstore = FAISS.from_texts(texts = textChunks, embedding = embeddings)
+
+    return vectorstore
 
 def main():
+    load_dotenv()
+
     st.set_page_config(page_title = "Chat With Multiple PDFs", page_icon = ":books:")
 
     st.header("Chat With Multiple PDFs :books:")
@@ -8,9 +39,15 @@ def main():
 
     with st.sidebar:
         st.subheader("Your documents")
-        st.file_uploader("Upload your pdfs here:")
-        st.button("Process")
+        pdfDocs = st.file_uploader("Upload your pdfs here:", accept_multiple_files = True)
+        
+        if st.button("Process"):
+            with st.spinner("Processing"):
+                rawText = getPdfText(pdfDocs)
+                
+                textChunks = getTextChunks(rawText)
 
+                vectorstore = getVectorstore(textChunks)
 
 if __name__ == "__main__":
     main()
