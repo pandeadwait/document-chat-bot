89dce3a5464760c9e6bd7772f203b56eb4612b7b
pandeadwait <pandeadwait21@gmail.com>
2025-07-27 12:41:55 +0530

    Changed the LLM model to gemini-2.5-flash and created conversation chains

--- app.py
+++ app.py
@@ -3,7 +3,10 @@ from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.vectorstores import FAISS
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from templates import css, bot_template, user_template
 import nest_asyncio
 
 nest_asyncio.apply()
@@ -32,13 +35,48 @@ def getVectorstore(textChunks):
 
     return vectorstore
 
+def getConversationChain(vectorstore):
+    llm = ChatGoogleGenerativeAI(model = "gemini-2.5-flash", temperature = 0.3)
+    memory = ConversationBufferMemory(memory_key = "chat_history", return_messages = True, input_key = "question")
+    conversationChain = ConversationalRetrievalChain.from_llm(
+        llm = llm,
+        retriever = vectorstore.as_retriever(),
+        memory = memory
+    )
+
+    return conversationChain
+
+def handleQuestion(question):
+    response = st.session_state.conversation({
+        'question' : question,
+        'chat_history': st.session_state.chatHistory
+    })
+
+    st.session_state.chatHistory = response['chat_history']
+
 def main():
     load_dotenv()
 
     st.set_page_config(page_title = "Chat With Multiple PDFs", page_icon = ":books:")
+    st.write(css, unsafe_allow_html = True)
 
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+
+    if "chatHistory" not in st.session_state:
+        st.session_state.chatHistory = []
+    
     st.header("Chat With Multiple PDFs :books:")
-    st.text_input("Ask a question about you document:")
+    question = st.text_input("Ask a question about you document:")
+    
+    for i, message in enumerate(st.session_state.chatHistory):
+        if i % 2 == 0:
+            st.write(user_template.replace("{{MSG}}", message.content), unsafe_allow_html = True)
+        else:
+            st.write(bot_template.replace("{{MSG}}", message.content), unsafe_allow_html = True)
+
+    if question:
+        handleQuestion(question)
 
     with st.sidebar:
         st.subheader("Your documents")
@@ -51,6 +89,8 @@ def main():
                 textChunks = getTextChunks(rawText)
 
                 vectorstore = getVectorstore(textChunks)
+
+                st.session_state.conversation = getConversationChain(vectorstore) 
 
 if __name__ == "__main__":
     main()
